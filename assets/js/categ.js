ca78dffb3a12c6d55a4224a25ac5f472231ee8a8
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-04-09 10:25:39 +0200

    ajout de correctifs pour les détails des films

--- assets/js/categ.js
+++ assets/js/categ.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('.add-to-cart');
+    const buttons = document.querySelectorAll('.ajout-panier'); 
 
     buttons.forEach(btn => {
         btn.addEventListener('click', function () {
@@ -15,7 +15,15 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(res => res.json())
             .then(data => {
                 if (data.success) {
-                    alert(data.message + " (Total dans le panier : " + data.total + ")");
+                    const cartCountSpan = document.getElementById('cart-count');
+                    if (cartCountSpan && data.total !== undefined) {
+                        cartCountSpan.textContent = data.total;
+                    }
+
+                    
+                    if (confirm(`${data.message} (Total dans le panier : ${data.total})\n\nSouhaitez-vous voir votre panier ? Cliquez sur OK pour voir votre panier !`)) {
+                        window.location.href = 'cart.php';
+                    }
                 } else {
                     alert("Erreur : " + data.message);
                 }
@@ -25,4 +33,14 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
+
+   
+    const toggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (toggle && navLinks) {
+        toggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    }
 });
