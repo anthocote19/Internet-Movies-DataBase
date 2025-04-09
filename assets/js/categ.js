fdd8472a03a14f0613111053bb1bca70fefc9aa2
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-04-09 10:24:45 +0200

    added navbar and footer on all the files

--- assets/js/categ.js
+++ assets/js/categ.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('.add-to-cart');
+    const buttons = document.querySelectorAll('.ajout-panier'); 
 
     buttons.forEach(btn => {
         btn.addEventListener('click', function () {
@@ -15,6 +15,12 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(res => res.json())
             .then(data => {
                 if (data.success) {
+                    const cartCountSpan = document.getElementById('cart-count');
+                    if (cartCountSpan && data.total !== undefined) {
+                        cartCountSpan.textContent = data.total;
+                    }
+
+                    
                     if (confirm(`${data.message} (Total dans le panier : ${data.total})\n\nSouhaitez-vous voir votre panier ? Cliquez sur OK pour voir votre panier !`)) {
                         window.location.href = 'cart.php';
                     }
@@ -27,4 +33,14 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
+
+   
+    const toggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (toggle && navLinks) {
+        toggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    }
 });
