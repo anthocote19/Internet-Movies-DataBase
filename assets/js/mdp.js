bf76cd1b29a785eb8b4bc6fd6290b86d103dd0a8
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-04-14 10:07:39 +0200

    added mdp.js

---
+++ assets/js/mdp.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    alert("Mot de passe changé avec succès, cliquez sur OK pour finir la procédure !");
+    setTimeout(() => {
+        window.location.href = "../index.php";
+    }, 3000);
+});
